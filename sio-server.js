e1748fa3d3c25437b03dfa1265d6052269740869
anant <anantrungta1999@gmail.com>
2018-10-01 23:30:45 +0530

    com

--- sio-server.js
+++ sio-server.js
@@ -1,19 +1,13 @@
 var express = require("express");
-var socket = require("socket.io");
-
 var http = require("http");
-var app = express().createServer();;
+var app = express();
 var config=require('./config');
-var io=socket.listen(app);
-
-io.configure () ->
-  io.set("transports", ["xhr-polling"])
-  io.set("polling duration", 10)
-
+var server = http.createServer(app).listen(config.port);
+var io = require("socket.io")(server);
 
 app.use(express.static("./public"));
 
-io.socket.on("connection", function(socket) {
+io.on("connection", function(socket) {
 
     socket.on("chat", function(message) {
         socket.broadcast.emit("message", message);
@@ -22,7 +16,5 @@ io.socket.on("connection", function(socket) {
     socket.emit("message", "Welcome to Cyber Chat");
 
 });
-port = process.env.PORT || 3000;
-app.listen(port);
 
 console.log("Starting Socket App - http://localhost:3000");
